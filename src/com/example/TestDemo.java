d5677c9c145d2d8145ddabe6f2ca2d9731f871c3
Nageswar <enageswararao@gmail.com>
2020-04-24 14:31:59 +0530

    frist comment

--- src/com/example/TestDemo.java
+++ src/com/example/TestDemo.java
@@ -5,7 +5,6 @@ public class TestDemo {
 	public static void main(String[] args) {
 		 
 		System.out.println("Test Demo Project......................");
-		System.out.println("Test Demo Project......................");
 
 	}
 
